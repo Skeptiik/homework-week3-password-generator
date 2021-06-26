4819800e56c07271b3c26abe654a0cd1f365da68
Jack <61893745+Skeptiik@users.noreply.github.com>
2021-06-26 15:54:06 +1000

    Add files via upload

--- script.js
+++ script.js
@@ -27,43 +27,26 @@ var passwd = '';
 function askParameters() {
   //asking if they want lowercase letters
   var lowercase = window.confirm("Would you like to add lowercase letters?");
-    if (lowercase === true) {
-      console.log("lowercase is: " + lowercase);
-    }
-      else { 
-        console.log("lowercase is: " + lowercase)
-      }
+    console.log("lowercase is: " + lowercase)
+
   //asking if they want uppercase letters
   var uppercase = window.confirm("Would you like to add uppercase letter?");
-    if (uppercase === true) {
-      console.log("uppercase is: " + uppercase);
-    } else {
-        console.log("uppercase is: " + uppercase);
-    }
+    console.log("uppercase is: " + uppercase);
   //asking if they want numbers
   var numbrs = window.confirm("Would you like to add numbers?");
-    if (numbrs === true) {
-      console.log("numbers is: " + numbrs);
-    } else {
-        console.log("numbers is: " + numbrs);
-    }
+    console.log("numbers is: " + numbrs);
   //asking if they want special characters
   var specialChars = window.confirm("Would you like to add special characters?")
-    if (specialChars === true) {
-      console.log("special characters is: " + specialChars);
-    } else {
-        console.log("Special characters is: " + specialChars);
-    }
+    console.log("special characters is: " + specialChars);
   //asking the user how long they want their password to be
   var length = window.prompt("How long would you like your password to be? (8-128)")
     if (length > 8 && length <128) {
       console.log("password length is: " + length);
-    } 
-    //giving the user a warning if they do not chose between 8-128 characters
-  if (length < 8 || length > 128) {
-    window.alert("your passwords needs to be between 8 and 128 characters long")
-    return
-  }
+    } else {  //giving the user a warning if they do not chose between 8-128 characters
+      window.alert("your passwords needs to be between 8 and 128 characters long")
+      return
+    }
+    
 }
   //code to generate the password
 
